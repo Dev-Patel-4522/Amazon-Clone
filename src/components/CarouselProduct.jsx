bc9b58c2d357aae57f10ab1e2fb97abef074e2d7
Dev-Patel-4522 <devpatel7524@gmail.com>
2024-01-29 17:35:03 +0530

    added functionalities

--- src/components/CarouselProduct.jsx
+++ src/components/CarouselProduct.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import { Link } from "react-router-dom"
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -7,23 +8,21 @@ import "swiper/css/navigation";
 const CarouselProduct = () => {
   return (
     <div className="bg-white m-3">
-        <div className="text-2xl font-semibold p-3">Best Seller</div>
-            <Swiper
-                slidesPerView={7}
-                spaceBetween={10}
-                navigation={true}
-                modules={[Navigation]}
-            >
-                {
-                    Array.from( { length: 9 }, (_, i) =>(
-                        <SwiperSlide key={i}>
-                            <img src={`../images/product_${i}_small.jpg`} alt="/"/>
-                        </SwiperSlide>
-
-                    ))
-                }
-
-            </Swiper>
+      <div className="text-2xl font-semibold p-3">Best Seller</div>
+      <Swiper
+        slidesPerView={7}
+        spaceBetween={10}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {Array.from({ length: 9 }, (_, i) => (
+          <SwiperSlide key={i}>
+            <Link to={`/product${i}`} >
+              <img src={`../images/product_${i}_small.jpg`} alt="/" />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Search } from "./"
 import { ShoppingCartIcon } from '@heroicons/react/24/outline'
 
 const NavBar = () => {
@@ -17,24 +18,33 @@ const NavBar = () => {
           </div>
         </div>
         {/* middle */}
-        <div className="flex">Middle</div>
+        <div className="flex grow relative items-center">
+          <Search />
+        </div>
         {/* right */}
         <div className="flex items-center">
           <div className="pr-4 pl-4">
             <div ClassName="text-xs xl:text-sm">Hello, sign in</div>
-            <div ClassName="text-sm xl:text-base font-bold">Account & Lists</div>
+            <div ClassName="text-sm xl:text-base font-bold">
+              Account & Lists
+            </div>
           </div>
           <div className="pr-4 pl-4">
             <div ClassName="text-xs xl:text-sm">Returns</div>
             <div ClassName="text-sm xl:text-base font-bold">& Orders</div>
           </div>
           <div className="flex pr-3 pl-3">
-            <ShoppingCartIcon className=" h-[48px]"/>
-            <div className="mt-7 text-xs xl:text-sm font-bold">
-                Cart
-            </div>
+            <ShoppingCartIcon className=" h-[48px]" />
+            <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
           </div>
         </div>
+      </div>
+      <div className="flex bg-amazonclone-light_blue text-white space-x-3 text-sm xl:text-sm p-2 pl-6">
+        <div>Today's Deals</div>
+        <div>Coustomer Service</div>
+        <div>Registiory</div>
+        <div>Gift Cards</div>
+        <div>Sell</div>
       </div>
     </header>
   );
